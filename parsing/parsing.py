c217268de4c0bcb325cbc61fc445b997bc5a0dc0
Nikita Fedorin <xfedorin@stuba.sk>
2024-12-22 22:35:09 +0100

    two ways of exctracting orig price

--- parsing/parsing.py
+++ parsing/parsing.py
@@ -1,4 +1,6 @@
 import json
+import pytz
+import re
 from selenium.webdriver.common.by import By
 from app_logger import logger
 from selenium.common.exceptions import SessionNotCreatedException
@@ -7,6 +9,8 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.remote.webelement import WebElement
 from parsing.init_driver import driver
 from typing import List
+from decimal import Decimal
+from datetime import datetime
 
 
 def find_parent_url(badge: WebElement) -> str | None:
@@ -43,34 +47,127 @@ def get_game_links(url: str) -> List[str]:
     return links
 
 
-def get_game_info(url: str) -> str:
+def get_game_title() -> str:
+    '''find game title at the page and return it'''
+    try:
+        wait = WebDriverWait(driver, 10)
+        title_block_locator = ".pdp-game-title"
+        wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, title_block_locator)))
+        title_block = driver.find_element(By.CSS_SELECTOR, title_block_locator) 
+        logger.info("Вижу название")
+        return title_block.text
+    except Exception as ex:
+        logger.error(f"Не получилось найти название игры: {ex}")
+        raise ex
+
+
+def get_game_price() -> Decimal:
+    '''Find actual price of game at page and return'''
+    try:
+        script = ("return document.querySelector"
+                  "('[data-qa=\"mfeCtaMain#offer0#finalPrice\"]');")
+
+        price_element = driver.execute_script(script)
+        price_raw = price_element.get_attribute("innerHTML")
+        price_clean = re.sub(r"&nbsp;|\s|TL", "", price_raw)
+        price_clean = price_clean.replace(".", "").replace(",", ".")
+        logger.debug(f"Цена: {price_clean}")
+        return Decimal(price_clean)
+    except Exception as ex:
+        logger.error(f"Не получилось найти цену игры: {ex}")
+        raise ex
+
+
+def get_discount_descriptor() -> datetime:
+    '''find discount descriptor at game's
+       page and return it'''
+    script = ("return document.querySelector"
+              "('[data-qa=\"mfeCtaMain#offer0#discountDescriptor\"]');")
+    try:
+        descriptor = driver.execute_script(script)
+        date_str = descriptor.text
+
+        date_str = date_str.replace("Offer ends ", "")
+        date_str = date_str.replace(" GMT+1", "")
+
+        date_format = "%m/%d/%Y %I:%M %p"
+
+        dt = datetime.strptime(date_str, date_format)
+
+        tz = pytz.timezone("Etc/GMT-1")
+        dt_with_tz = tz.localize(dt)
+        logger.debug(f"Скидка заканчивается: {dt_with_tz}")
+        return dt_with_tz
+    except Exception as ex:
+        logger.error(f"Не могу найти дату окончания скидки: {ex}")
+        raise ex
+
+
+def get_game_original_price() -> Decimal:
+    '''Find original price at the game's 
+       page and return it'''
+    try:
+        price_locator = '[data-qa="mfeCtaMain#offer0#originalPrice"]'
+        
+        logger.debug("Жду загрузки прошлой цены")
+        wait = WebDriverWait(driver, 10)
+        wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, price_locator)))
+        logger.debug("Прошлая цена прогрузилась") 
+        script = f"return document.querySelector('{price_locator}');"
+        price_element = driver.find_element(By.CSS_SELECTOR, price_locator)
+        
+        price_raw = price_element.text
+        logger.debug(f"price_raw: {price_raw}")
+         
+
+        if not price_raw:
+            logger.error("Элемент с ценой не найден")
+            price_raw = price_element.get_attribute("innerHTML").split("</span>")[2]
+            logger.debug(f"price_raw: {price_raw}")
+        
+        if price_raw:
+            price_clean = re.sub(r"&nbsp;|\s|TL", "", price_raw)
+            price_clean = price_clean.replace(".", "").replace(",", ".")
+            logger.debug(f"Исходная цена: {price_clean}")
+        
+            return Decimal(price_clean)
+        else:
+            return Decimal(0)
+    except Exception as ex:
+        logger.error(f"Не получилось найти прошлую цену игры: {ex}")
+        raise ex
+
+
+def get_game_info(url: str) -> dict:
     '''Append product info to json file'''
+    game_data = {}
     try:
         driver.get(url)
         logger.info("Открыл страницу с игрой")
-        wait = WebDriverWait(driver, 10)
-        product_locator = ".pdp-game-title"
-        wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, product_locator)))
-        product_element = driver.find_element(By.CSS_SELECTOR, product_locator) 
-        logger.info("Вижу название")
-        return product_element.text
+        title = get_game_title()
+        price = get_game_price()
+        original_price = get_game_original_price()
+        discount_descriptor = get_discount_descriptor()
+        game_data["title"] = title
+        game_data["price"] = price
+        game_data["original_price"] = original_price
+        game_data["discount_descriptor"] = discount_descriptor
+        return game_data
     except Exception as ex:
         logger.error(f"Не удалось получить информацию"
-                      "о продукте {url}: {ex}")
+                      f"о продукте {url}: {ex}")
         raise ex
 
 
 def parse_games() -> None:
     '''Parse actual games list in PSN-store and save it to json'''
-    data=[]
+    game_data=[]
     page_url = "https://store.playstation.com/en-tr/category/83a687fe-bed7-448c-909f-310e74a71b39/1"
     try:
         with open("product_data.json", "w", encoding="utf-8") as json_file:
             for link in get_game_links(page_url):
-                game_title = get_game_info(link)    
-                data.append({"url": link,
-                             "title": game_title})
-            json.dump(data, json_file, ensure_ascii=False, indent=4)
+                game_data.append(get_game_info(link))
+            json.dump(game_data, json_file, ensure_ascii=False, indent=4)
         logger.info(f"Запись данных в JSON завершена")
         driver.quit()
     except Exception as ex:
